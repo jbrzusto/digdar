e6b1fa8d87fd491e2f3719292f7311698030dfb9
john <jbrzusto@fastmail.fm>
2014-08-08 16:17:20 +0000

    streamline trigger generator

--- FPGA/release1/fpga/code/rtl/trigger_gen.v
+++ FPGA/release1/fpga/code/rtl/trigger_gen.v
@@ -7,211 +7,158 @@
  *
  * (c) 2014 John Brzustowski https://radr-project.org
  *
- * This part of code is written in Verilog hardware description language (HDL).
- * Please visit http://en.wikipedia.org/wiki/Verilog
- * for more details on the language used herein.
-
-   Generate a trigger a fixed delay after the signal crosses an excitation
-   threshold after having crossed a relaxation threshold.  The threshold
-   values are independent, and when unequal, "crossing" a threshold means
-   being found at or beyond it in the direction away from the other threshold.
-   If the thresholds are equal, a positive crossing is treated as excitation,
-   a negative crossing as relaxation.
-   If a non-zero latency is specified, then at least that number of clock ticks 
-   must elapse between consecutive excitation events (and a relaxation event is
-   still required between these).
-   The count of ticks since the most recent excitation is maintained in register "age".
-   The delay value does not affect the sequence of "age" values, but
-   is merely the age at which an excitation causes output "trigger" to be asserted.
-  
-   Timing:  if the input at clock pulse N crosses the excitation threshold after
-   a crossing of the relaxation threshold, then the trigger output is high
-   at the start of the clock pulse N+1+delay.
-  
-   The trigger is only held high for one clock.
-  
+ *
+ * Generate a trigger a fixed delay after the signal crosses an
+ * excitation threshold after having crossed a relaxation threshold.
+ *
+ * The threshold values are independent, and when unequal, "crossing"
+ * a threshold means being found at or beyond it in the direction away
+ * from the other threshold.  If the thresholds are equal, a positive
+ * crossing is treated as excitation, a negative crossing as
+ * relaxation.
+ *
+ * If a non-zero latency is specified, then at least that number of
+ * clock ticks must elapse between consecutive excitation events (and
+ * a relaxation event is still required between these).  Actual latency
+ * is 2 plus the specified value.
+ *
+ * The count of ticks since the most recent excitation is maintained
+ * in register "age".  The delay value does not affect the sequence of
+ * "age" values, but is merely the age at which an excitation causes
+ * output "trigger" to be asserted.
+ *
+ * Timing: if the input at clock pulse N crosses the excitation
+ * threshold after a crossing of the relaxation threshold and any
+ * required latency, then the trigger output is high at the start of
+ * the clock pulse N+1+delay.
+ *
+ * Each trigger is held high for one clock.
+ * 
  */
 
-//  mode M1:  thresh_relax < thresh_excite (usual situation)
+// mode of triggering depends on ordering of relax and excite thresholds:
 
-`define TS_M1_WAITING_RELAX     3'd0  // waiting for signal to cross threshold in relaxation direction
-`define TS_M1_WAITING_EXCITE    3'd1  // waiting for signal to cross threshold in excitation direction
+`define MODE_NORMAL             2'd0  // thresh_relax < thresh_excite (usual situation)
+`define MODE_INVERTED           2'd1  // thresh_relax > thresh_excite (inverted signal)
+`define MODE_NOHYSTERESIS       2'd2  // thresh_relax == thresh_excite (single threshold; crossing upward is excitation)
 
-// mode M2:  thresh_relax > thresh_excite (inverted signal)
+// current state is waiting for either relax or excite, or delaying
 
-`define TS_M2_WAITING_RELAX     3'd2  // waiting for signal to cross threshold in relaxation direction
-`define TS_M2_WAITING_EXCITE    3'd3  // waiting for signal to cross threshold in excitation direction
-
-// mode M3:  thresh_relax == thresh_excite (single threshold; crossing upward is excitation)
-
-`define TS_M3_WAITING_RELAX     3'd4  // waiting for signal to cross threshold in relaxation direction
-`define TS_M3_WAITING_EXCITE    3'd5  // waiting for signal to cross threshold in excitation direction
-
-`define TS_DELAYING             3'd6  // threshold crossed; delaying before triggering
+`define STATE_WAITING_RELAX     2'd0  // waiting for signal to cross threshold in relaxation direction
+`define STATE_WAITING_EXCITE    2'd1  // waiting for signal to cross threshold in excitation direction
+`define STATE_DELAYING          2'd2  // threshold crossed; delaying before triggering
 
 module trigger_gen 
   ( input clock,
-    input reset,
-    input enable,
-    input [width-1:0] signal_in,
-    input [width-1:0] thresh_relax,
-    input [width-1:0] thresh_excite,
-    input [delay_width-1:0] delay,
-    input [age_width-1:0] latency,
-    output reg 		    trigger,
+    input                          reset,
+    input                          enable,
+    input [width-1:0]              signal_in,
+    input [width-1:0]              thresh_relax,
+    input [width-1:0]              thresh_excite,
+    input [delay_width-1:0]        delay,
+    input [age_width-1:0]          latency,
+    output reg                     trigger,
     output reg [counter_width-1:0] counter
     );
    
-   parameter width = 12;
-   parameter delay_width = 32;
+   parameter width         = 12;
+   parameter delay_width   = 32;
    parameter counter_width = 32;
-   parameter age_width = 32;
-   parameter do_smoothing = 1;
+   parameter age_width     = 32;
+   parameter do_smoothing  = 1;
    
-   reg [2:0] 			   state;
-   reg [delay_width-1:0] 	   delay_counter;
-   reg [width-1:0] 		   sig_smoothed; // possibly smoothed version of signal_in
-   reg [age_width-1:0]             age;          // number of clock ticks since reset or assertion of trigger
-   reg [age_width-1:0]             latency_counter;  // number of clock ticks remaining after excite before next excite is allowed
-   
-   // smooth the signal with an IIR filter:  smooth[i+1] <= (7 * smooth[i] + value[i+1]) / 8
-   // FIXME: make the weighting a control parameter, instead of hardwiring 7/8, 1/8.
-   // Note the sign extension, as we treat signal as signed.
+   reg [2-1:0] 			   state;           // one of STATE values above
+   reg [delay_width-1:0] 	   delay_counter;   // countdown wait between trigger detection and assertion
+   reg [width-1:0] 		   sig_smoothed;    // possibly smoothed version of signal_in
+   reg [age_width-1:0]             age;             // number of clock ticks since assertion of trigger
 
-   reg [width + 3 - 1: 0]          smoother;
+   reg [2-1:0]                     mode;            // one of MODE values above
+
+   wire                            excited;         // true when smoothed signal is at or beyond excitation threshold, away from relaxation
+   wire                            relaxed;         // true when smoothed signal is at or beyond relaxation threshold, away from excitation
    
+    
+   reg [width + 3 - 1: 0]          smoother;
+
+   always @(posedge clock)
+     begin
+        if ($signed(thresh_relax) < $signed(thresh_excite))
+          mode <= `MODE_NORMAL;
+        else if ($signed(thresh_relax) > $signed(thresh_excite))
+          mode <= `MODE_INVERTED;
+        else
+          mode <= `MODE_NOHYSTERESIS;
+     end
+
+   assign excited = ((mode == `MODE_NORMAL)       && $signed(sig_smoothed) >= $signed(thresh_excite)) ||
+                    ((mode == `MODE_INVERTED)     && $signed(sig_smoothed) <= $signed(thresh_excite)) ||
+                    ((mode == `MODE_NOHYSTERESIS) && $signed(sig_smoothed) >  $signed(thresh_excite));
+
+   assign relaxed = ((mode == `MODE_NORMAL)       && $signed(sig_smoothed) <= $signed(thresh_relax)) ||
+                    ((mode == `MODE_INVERTED)     && $signed(sig_smoothed) >= $signed(thresh_relax)) ||
+                    ((mode == `MODE_NOHYSTERESIS) && $signed(sig_smoothed) <  $signed(thresh_relax));
+     
    always @(posedge clock)
      if (reset | ~enable)
        begin
-	  if ($signed(thresh_relax) < $signed(thresh_excite))
-	    state <=  `TS_M1_WAITING_RELAX;
-	  else if ($signed(thresh_relax) > $signed(thresh_excite))
-	    state <=  `TS_M2_WAITING_RELAX;
-	  else
-	    state <=  `TS_M3_WAITING_RELAX;
+          state <= `STATE_WAITING_EXCITE;
 	  trigger <=  1'b0;
 	  counter <=  1'b0;
 	  sig_smoothed <=  signal_in;
           smoother <= {signal_in, 3'b0};
-	  age <=  1'b0;
-          latency_counter <= 1'b0;
+	  age <=  latency;  // so that we don't wait for latency before the very first trigger
        end
      else
        begin	  
 	  age <= age + 1'b1;
 
-          smoother[width + 3 - 1: 0] = {sig_smoothed[width-1:0], 3'b0} - {{3{sig_smoothed[width - 1]}}, sig_smoothed[width-1:0]} + {{3{signal_in[width - 1]}}, signal_in[width - 1:0]};
+   // smooth the signal with an IIR filter:  smooth[i+1] <= (7 * smooth[i] + value[i+1]) / 8
+   // FIXME: make the weighting a control parameter, instead of hardwiring 7/8, 1/8.
+   // Note the sign extension, as we treat signal as signed.
 
-	  sig_smoothed =  do_smoothing ? smoother[3 + width - 1 : 3] : signal_in;
+          smoother[width + 3 - 1: 0] <= {sig_smoothed[width-1:0], 3'b0} - {{3{sig_smoothed[width - 1]}}, sig_smoothed[width-1:0]} + {{3{signal_in[width - 1]}}, signal_in[width - 1:0]};
 
-	  if (|latency_counter) 
-            begin
-               // count down latency
-               latency_counter <= latency_counter - 1'b1;
-            end
-	  
+	  sig_smoothed <= do_smoothing ? smoother[3 + width - 1 : 3] : signal_in;
+
 	  case (state)
-	    `TS_M1_WAITING_RELAX:
+	    `STATE_WAITING_RELAX:
 	      begin
 		 trigger <=  1'b0;
-		 if ($signed(sig_smoothed) <= $signed(thresh_relax))
+		 if (relaxed)
 		   begin
-		      state <=  `TS_M1_WAITING_EXCITE;
+		      state <=  `STATE_WAITING_EXCITE;
 		   end
 	      end
-	    
-	    `TS_M1_WAITING_EXCITE:
+                        
+	    `STATE_WAITING_EXCITE:
 	      begin
-                 if ((! (| latency_counter)) && $signed(sig_smoothed) >= $signed(thresh_excite))
+                 if (excited & age >= latency)
 		   begin
 		      age <=  1'b0;
 		      counter <=  counter + 1'b1;
-                      latency_counter <= latency;
 		      if (delay == 0)
 			begin
-			   state <=  `TS_M1_WAITING_RELAX;
+			   state <=  `STATE_WAITING_RELAX;
 			   trigger <=  1'b1;
 			end
 		      else
 			begin
-			   state <=  `TS_DELAYING;
+			   state <=  `STATE_DELAYING;
 			   delay_counter <=  delay;
 			end
 		   end
               end
 	    
-	    `TS_M2_WAITING_RELAX:
-	      begin
-		 trigger <=  1'b0;
-		 if ($signed(sig_smoothed) >= $signed(thresh_relax))
-		   begin
-		      state <=  `TS_M2_WAITING_EXCITE;
-		   end
-	      end
-	    
-	    `TS_M2_WAITING_EXCITE:
-	      begin
-		 if ((! (|latency_counter)) && $signed(sig_smoothed) <= $signed(thresh_excite))
-                        begin
-		           age <=  1'b0;
-		           counter <=  counter + 1'b1;
-                           latency_counter <= latency;
-		           if (delay == 0)
-			     begin
-			        state <=  `TS_M2_WAITING_RELAX;
-			        trigger <=  1'b1;
-			     end
-		           else
-			     begin
-			        state <=  `TS_DELAYING;
-			        delay_counter <=  delay;
-			     end
-		        end
-              end
-	    
-	    `TS_M3_WAITING_RELAX:
-	      begin
-		 trigger <=  1'b0;
-		 if ($signed(sig_smoothed) < $signed(thresh_relax))
-		   begin
-		      state <=  `TS_M3_WAITING_EXCITE;
-		   end
-	      end
-	    
-	    `TS_M3_WAITING_EXCITE:
-	        begin
-		   if ((! (|latency_counter)) && $signed(sig_smoothed) > $signed(thresh_excite))
-		     begin
-		        age <=  1'b0;
-		        counter <=  counter + 1'b1;
-                        latency_counter <= latency;
-		        if (delay == 0)
-			  begin
-			     state <=  `TS_M3_WAITING_RELAX;
-			     trigger <=  1'b1;
-			  end
-		        else
-			  begin
-			     state <=  `TS_DELAYING;
-			     delay_counter <=  delay;
-			  end
-		     end
-	        end
-	    `TS_DELAYING:
+	    `STATE_DELAYING:
               if (| delay_counter)
                 begin
 		   delay_counter <= delay_counter - 1'b1;
 		end
               else
-		begin
-		   if ($signed(thresh_relax) < $signed(thresh_excite))
-		     state <=  `TS_M1_WAITING_RELAX;
-		   else if ($signed(thresh_relax) > $signed(thresh_excite))
-		     state <=  `TS_M2_WAITING_RELAX;
-		   else
-		     state <=  `TS_M3_WAITING_RELAX;
+                begin
+		   state <=  `STATE_WAITING_RELAX;
 		   trigger <=  1'b1;
 		end
-	  endcase // case (state)
+          endcase
        end
 endmodule // red_pitaya_trigger_gen
