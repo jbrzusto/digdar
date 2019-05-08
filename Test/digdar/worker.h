70fbb3a5ceaf59ee23486a730ed0a4176fb38dc4
john brzustowski <jbrzusto@fastmail.fm>
2019-05-08 13:28:19 +0000

    add file missed from previous commit re acp count scaling removals

--- Test/digdar/worker.h
+++ Test/digdar/worker.h
@@ -1,10 +1,10 @@
 /**
  * forked from:
- * 
+ *
  * @brief Red Pitaya Oscilloscope worker module.
- * 
+ *
  * @Author Jure Menart <juremenart@gmail.com>
- * 
+ *
  * (c) Red Pitaya  http://www.redpitaya.com
  *
  * This part of code is written in C programming language.
@@ -35,13 +35,13 @@ extern digdar_fpga_reg_mem_t *g_digdar_fpga_reg_mem;
  */
 typedef enum rp_osc_worker_state_e {
     /** do nothing, idling */
-    rp_osc_idle_state = 0, 
+    rp_osc_idle_state = 0,
     /** requests an shutdown of the worker thread */
     rp_osc_quit_state,
     /* abort current measurement */
     rp_osc_start_state,
-    /* auto mode acquisition - continuous measurements without trigger */ 
-    rp_osc_running_state, 
+    /* auto mode acquisition - continuous measurements without trigger */
+    rp_osc_running_state,
     /* non existing state - just to define the end of enumeration - must be
      * always last in the enumeration */
     rp_osc_nonexisting_state
@@ -70,11 +70,12 @@ extern uint16_t num_pulses; // pulses to maintain in ring buffer (filled by work
 extern uint16_t chunk_size; // pulses to transmit per chunk (main thread)
 extern uint16_t num_chunks; // number of chunks in ring buffer
 extern uint16_t *pulses_in_chunk; // number of pulses in each buffer chunk
-extern uint32_t psize; // size of each pulse's storage
+  extern uint32_t psize; // size of each pulse's storage
+  extern uint16_t acps; // acp pulses per sweep as specified by user
 
 typedef struct {
-  float begin;
-  float end;
+  uint16_t begin;
+  uint16_t end;
 } sector;
 
 #define MAX_REMOVALS 32
